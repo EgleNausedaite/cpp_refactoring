0c915a05b69febe446998cb7df98361d68fdd1bf
philproudman <phil.proudman@gmail.com>
2026-09-23 16:03:34 +0100

    TCR: tests passing

--- src/longparameterlist/ShippingNoteGenerator.cpp
+++ src/longparameterlist/ShippingNoteGenerator.cpp
@@ -2,6 +2,15 @@
 
 namespace refactoring::longparameterlist {
 
+class Customer {
+public:
+        Customer(const std::string& firstName, const std::string& lastName) :
+        m_firstName(firstName), m_lastName(lastName) {
+        }
+        std::string m_firstName;
+        std::string m_lastName;
+};
+
 std::string ShippingNoteGenerator::generateShippingNote(
         const std::string& customerFirstName,
         const std::string& customerLastName,
