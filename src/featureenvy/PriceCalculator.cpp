3f04eec59a90034b5375142a1e09fcb395226477
Egle-the-Eagle <egle.nausedaite@sky.uk>
2026-09-23 15:49:43 +0100

    TCR: tests passing

--- src/featureenvy/PriceCalculator.cpp
+++ src/featureenvy/PriceCalculator.cpp
@@ -3,17 +3,9 @@
 namespace refactoring::featureenvy {
 
 double PriceCalculator::calculateFinalPrice(const Product& product) const {
-    return calculateProductFinalPrice(product);
+    return product.calculateProductFinalPrice(product);
 }
 
-    double PriceCalculator::calculateProductFinalPrice(const Product &product) const {
-        double price = product.getPrice();
 
-        if (product.isOnSale()) {
-            price = price * 0.8;
-        }
-
-        return price;
-    }
 
 } // namespace refactoring::featureenvy
--- src/featureenvy/Product.h
+++ src/featureenvy/Product.h
@@ -9,6 +9,16 @@ public:
     double getPrice() const;
     bool isOnSale() const;
 
+    double calculateProductFinalPrice(const Product &product) const {
+        double price = product.getPrice();
+
+        if (product.isOnSale()) {
+            price = price * 0.8;
+        }
+
+        return price;
+    }
+
 private:
     double price_;
     bool onSale_;
